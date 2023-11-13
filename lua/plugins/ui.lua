8f5b4e9ce6aebacf88bab9a07eee39da485b0a69
Lee Henson <lee.m.henson@gmail.com>
2023-11-13 14:12:42 +0000

    wip

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        animation = function(s, n)
+          return 5
+        end,
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-notify",
+    enabled = false,
+  },
+}
